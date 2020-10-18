7463fc8d634f859c972498dc0088a9c36d22f1b2
Andre Silva <1137236@sga.pucminas.br>
2020-10-18 16:29:51 -0300

    serviços de cadastro e login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiSOSPet;
+using System.Net;
+
+namespace ApiSOSPet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly SospetContext _context;
+
+        public LoginController()
+        {
+            _context = new SospetContext();
+        }
+
+        // GET: api/Login
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        // GET: api/Login/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(long id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // PUT: api/Login/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuario(long id, Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Login
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Auth credenciais)
+        {
+            var usuario = _context.Usuarios.Where(e => e.Email == credenciais.Email).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return BadRequest(
+                       new
+                       {
+                           ErrMsg = "Usuario não encontrado",
+                           err = true
+                       });
+            }
+            else if(usuario.Senha != credenciais.Senha)
+            {
+                return BadRequest(
+                        new
+                        {
+                            ErrMsg = "Senha Incorreta",
+                            err = true
+                        });
+            }
+            else
+            {
+                return StatusCode(200);
+            }
+            
+           
+           
+        }
+
+        // DELETE: api/Login/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> DeleteUsuario(long id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        private bool UsuarioExists(long id)
+        {
+            return _context.Usuarios.Any(e => e.Id == id);
+        }
+    }
+}
