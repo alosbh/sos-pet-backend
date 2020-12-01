94d267c1fb51d61f7e06542db2c52580dc09c0a8
rafaelsiq <rafaelsqf22@gmail.com>
2020-12-01 19:54:36 -0300

    inclusao de count

--- Controllers/OcorrenciasController.cs
+++ Controllers/OcorrenciasController.cs
@@ -44,33 +44,6 @@ namespace ApiSOSPet.Controllers
             return ocorrencia;
         }
 
-        // GET: api/Ocorrencias/usuario=1
-        [HttpGet("usuario={id}")]
-        public async Task<ActionResult<IEnumerable<Ocorrencia>>> GetOcorrencia(long? id)
-        {
-
-            var ocorrencia = await _context.Ocorrencias.ToListAsync();
-            var ocorrencia2 = _context.Ocorrencias.ToList();
-    
-            if (ocorrencia == null)
-            {
-                return NotFound();
-            }
-
-            ocorrencia2.Clear();
-            
-
-            foreach (var item in ocorrencia)
-            {
-
-                if (item.Idusuario == id)
-                {
-                    ocorrencia2.Add(item);
-                }
-                    
-            }
-            return ocorrencia2;
-        }
         // PUT: api/Ocorrencias/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
@@ -131,6 +104,69 @@ namespace ApiSOSPet.Controllers
             return ocorrencia;
         }
 
+        // GET: api/Ocorrencias/usuario=1
+        [HttpGet("usuario={id}")]
+        public async Task<ActionResult<IEnumerable<Ocorrencia>>> GetOcorrencia(long? id)
+        {
+
+            var ocorrencia = await _context.Ocorrencias.ToListAsync();
+            var ocorrencia2 = _context.Ocorrencias.ToList();
+
+            if (ocorrencia == null)
+            {
+                return NotFound();
+            }
+
+            ocorrencia2.Clear();
+
+
+            foreach (var item in ocorrencia)
+            {
+
+                if (item.Idusuario == id)
+                {
+                    ocorrencia2.Add(item);
+                }
+
+            }
+            return ocorrencia2;
+        }
+        
+
+
+
+
+        // GET: api/Ocorrencias/status=
+        [HttpGet("status={id}")]
+        public async Task<string> GetOcorrencia(string id)
+        {
+
+            var ocorrencia = await _context.Ocorrencias.ToListAsync();
+            var ocorrencia2 = _context.Ocorrencias.ToList();
+
+            if (ocorrencia == null)
+            {
+                Microsoft.AspNetCore.Mvc.NotFoundResult notFoundResult = new Microsoft.AspNetCore.Mvc.NotFoundResult();
+                return notFoundResult.ToString();
+            }
+
+            ocorrencia2.Clear();
+            int total = 0;
+
+            foreach (var item in ocorrencia)
+            {
+
+                if (item.Status.ToLower() == id)
+                {
+                    total++;
+                }
+
+            }
+
+            return "{" + "total" + '"' + ":" + '"' + total + '"' + "}";
+        }
+
+        
         private bool OcorrenciaExists(long id)
         {
             return _context.Ocorrencias.Any(e => e.Idocorr == id);
